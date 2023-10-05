36f935e5747ae712b33b2a436e0530bfe5dc5da5
Yixuan Qiu <yixuanq@gmail.com>
2023-10-05 16:56:28 +0800

    add example for SVM

--- R/svm.R
+++ R/svm.R
@@ -32,6 +32,38 @@
 ##'
 ##'         Ben Dai \url{https://bendai.org}
 ##'
+##' @examples
+##' # Simulate a data set
+##' set.seed(123)
+##' n = 5000
+##' p = 100
+##' x1 = matrix(rnorm(n / 2 * p, -0.25, 0.1), n / 2)
+##' x2 = matrix(rnorm(n / 2 * p, 0.25, 0.1), n / 2)
+##' x = rbind(x1, x2)
+##' beta = 0.1 * rnorm(p)
+##' prob = plogis(c(x %*% beta))
+##' y = 2 * rbinom(n, 1, prob) - 1
+##' C = 0.5
+##'
+##' # Compute using ReHLine
+##' time1 = system.time(
+##'     res1 <- svm(x, y, C = C * n, max_iter = 1000, tol = 1e-3, verbose = 0)
+##' )
+##' beta1 = res1$beta
+##' print(time1)
+##'
+##' # Compare the result with LiblineaR
+##' if (require(LiblineaR))
+##' {
+##'     time2 = system.time(
+##'         res2 <- LiblineaR(x, y, type = 3, cost = C, epsilon = 1e-3, bias = 0, verbose = FALSE)
+##'     )
+##'     beta2 = as.numeric(res2$W)
+##'     print(time2)
+##'
+##'     plot(beta1, beta2)
+##'     abline(0, 1, col = "red")
+##' }
 svm = function(x, y, C = 1, max_iter = 1000, tol = 1e-5, shrink = TRUE, verbose = 0)
 {
     n = nrow(x)
@@ -41,5 +73,5 @@ svm = function(x, y, C = 1, max_iter = 1000, tol = 1e-5, shrink = TRUE, verbose 
 
     res = rehline(x, Umat, Vmat, max_iter = max_iter,
                   tol = tol, shrink = shrink, verbose = verbose)
-    res$beta
+    res
 }
